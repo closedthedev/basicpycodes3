96b5e2bc4bd7808bfb3201b3adc2742c856246d3
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-05-02 23:56:52 -0300

    new code

---
+++ listas1.py
@@ -0,0 +1,12 @@
+#primeiros comandos básicos de uma lista
+
+lista_de_comidas = ['Arroz' , 'Feijão' , 'Linguiça' , 'Macarrão']
+lista_de_comidas.append('Churrasco') #adicionando mais um valor na variável
+lista_de_comidas[2] = 'Maionese' #trocando o número 2 da lista por maionese, que no caso é a palavra Linguiça
+
+print(lista_de_comidas)
+
+lista_de_comidas.pop(0) #excluindo o número 0 na lista, que no caso é o Arroz
+
+print(lista_de_comidas)
+
