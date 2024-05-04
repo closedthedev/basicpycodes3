8a158fd6005b2ebd44258f5c2f5f1892fb192a3e
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-05-04 16:27:43 -0300

    att

--- listas9.py
+++ listas9.py
@@ -1,6 +1,6 @@
 qnt_pessoas = int(input('Digite a quantidade de pessoas que tem na sua família: '))
 familia = []
-
+totmenos = totmais = 0
 for i in range(qnt_pessoas):
     print('--' * 10, f'{i+1}ª PESSOA', '--' * 10)
     nome = input('Nome: ')
@@ -8,4 +8,10 @@ for i in range(qnt_pessoas):
     pessoa = [nome, idade]  # criando uma lista com os dados da pessoa, e jogando para a lista principal logo em seguida
     familia.append(pessoa)
 
+for p in familia:
+    if p[1] >= 18:
+        totmais += 1
+    else: totmenos +=1
+
 print(familia)
+print(f'Na sua família, {totmais} pessoas são maiores de idade, enquanto {totmenos} ainda não são!') 
