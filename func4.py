ac29c9a148b7c6b2ec238e718b7b4afdbe5a80e2
closedthedev <104802109+closedthedev@users.noreply.github.com>
2024-06-15 11:43:18 -0300

    new code

---
+++ func4.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+
+    tamanho_palavra = len(msg)
+    print('-'*(int(tamanho_palavra*2.5)))
+    print(f' '*(int(tamanho_palavra/2)) ,msg,)
+    print('-'*(int(tamanho_palavra*2.5)))
+
+escreva('zerara')
+
+
+
